fec024a612363eecf5cf3d71ef3016bc0a9fe7f3
nisrina salsa <nisrinahikmawan@gmail.com>
2024-11-28 20:30:03 +0700

    first commit

---
+++ brick_score_sound.py
@@ -0,0 +1,229 @@
+import tkinter as tk
+import pygame
+
+pygame.mixer.init()
+
+class GameObject(object):
+    def __init__(self, canvas, item):
+        self.canvas = canvas
+        self.item = item
+
+    def get_position(self):
+        return self.canvas.coords(self.item)
+
+    def move(self, x, y):
+        self.canvas.move(self.item, x, y)
+
+    def delete(self):
+        self.canvas.delete(self.item)
+
+
+class Ball(GameObject):
+    def __init__(self, canvas, x, y):
+        self.radius = 10
+        self.direction = [1, -1]
+        self.speed = 5 
+        item = canvas.create_oval(x - self.radius, y - self.radius,
+                                  x + self.radius, y + self.radius,
+                                  fill='white')
+        super(Ball, self).__init__(canvas, item)
+
+    def update(self):
+        coords = self.get_position()
+        width = self.canvas.winfo_width()
+        if coords[0] <= 0 or coords[2] >= width:
+            self.direction[0] *= -1
+        if coords[1] <= 0:
+            self.direction[1] *= -1
+        x = self.direction[0] * self.speed
+        y = self.direction[1] * self.speed
+        self.move(x, y)
+
+    def collide(self, game_objects):
+        coords = self.get_position()
+        x = (coords[0] + coords[2]) * 0.5
+        if len(game_objects) > 1:
+            self.direction[1] *= -1
+        elif len(game_objects) == 1:
+            game_object = game_objects[0]
+            coords = game_object.get_position()
+            if x > coords[2]:
+                self.direction[0] = 1
+            elif x < coords[0]:
+                self.direction[0] = -1
+            else:
+                self.direction[1] *= -1
+
+        for game_object in game_objects:
+            if isinstance(game_object, Brick):
+                game_object.hit()
+                self.play_sound()  # Play the sound when the ball hits a brick
+
+    def play_sound(self):
+        """Function to play the sound when the ball hits a brick"""
+        pygame.mixer.Sound("pop.mp3").play()  # Play the sound file
+
+class Paddle(GameObject):
+    def __init__(self, canvas, x, y):
+        self.width = 80
+        self.height = 10
+        self.ball = None
+        item = canvas.create_rectangle(x - self.width / 2,
+                                       y - self.height / 2,
+                                       x + self.width / 2,
+                                       y + self.height / 2,
+                                       fill='#FFB643')
+        super(Paddle, self).__init__(canvas, item)
+
+    def set_ball(self, ball):
+        self.ball = ball
+
+    def move(self, offset):
+        coords = self.get_position()
+        width = self.canvas.winfo_width()
+        if coords[0] + offset >= 0 and coords[2] + offset <= width:
+            super(Paddle, self).move(offset, 0)
+            if self.ball is not None:
+                self.ball.move(offset, 0)
+
+
+class Brick(GameObject):
+    COLORS = {1: '#FFDE59', 2: '#FE9900', 3: '#D20103'}
+
+    def __init__(self, canvas, x, y, hits):
+        self.width = 75
+        self.height = 20
+        self.hits = hits
+        color = Brick.COLORS[hits]
+        item = canvas.create_rectangle(x - self.width / 2,
+                                       y - self.height / 2,
+                                       x + self.width / 2,
+                                       y + self.height / 2,
+                                       fill=color, tags='brick')
+        super(Brick, self).__init__(canvas, item)
+
+    def hit(self):
+        self.hits -= 1
+        if self.hits == 0:
+            self.delete()
+            self.canvas.game.score += 10  # Tambah skor
+            self.canvas.game.update_score_text()
+        else:
+            self.canvas.itemconfig(self.item, fill=Brick.COLORS[self.hits])
+
+
+class Game(tk.Frame):
+    def __init__(self, master):
+        super(Game, self).__init__(master)
+        self.lives = 3
+        self.score = 0
+        self.width = 610
+        self.height = 400
+        self.canvas = tk.Canvas(self, bg='#FFECA1',
+                                width=self.width,
+                                height=self.height)
+        self.canvas.pack()
+        self.pack()
+        self.canvas.game = self  # Referensi ke game
+
+        self.items = {}
+        self.ball = None
+        self.paddle = Paddle(self.canvas, self.width / 2, 326)
+        self.items[self.paddle.item] = self.paddle
+
+        for x in range(5, self.width - 5, 75):
+            self.add_brick(x + 37.5, 50, 3)
+            self.add_brick(x + 37.5, 70, 2)
+            self.add_brick(x + 37.5, 90, 1)
+
+        self.hud = None
+        self.setup_game()
+        self.canvas.focus_set()
+        self.canvas.bind('<Left>', lambda _: self.paddle.move(-10))
+        self.canvas.bind('<Right>', lambda _: self.paddle.move(10))
+
+    def setup_game(self):
+        self.add_ball()
+        self.update_lives_text()
+        self.update_score_text()
+        self.text = self.draw_text(300, 200, 'Press Space to start')
+        self.canvas.bind('<space>', lambda _: self.start_game())
+
+    def add_ball(self):
+        if self.ball is not None:
+            self.ball.delete()
+        paddle_coords = self.paddle.get_position()
+        x = (paddle_coords[0] + paddle_coords[2]) * 0.5
+        self.ball = Ball(self.canvas, x, 310)
+        self.paddle.set_ball(self.ball)
+
+    def add_brick(self, x, y, hits):
+        brick = Brick(self.canvas, x, y, hits)
+        self.items[brick.item] = brick
+
+    def draw_text(self, x, y, text, size='40'):
+        font = ('Forte', size)
+        return self.canvas.create_text(x, y, text=text,
+                                       font=font)
+
+    def update_lives_text(self):
+        text = f'Lives: {self.lives}'
+        if hasattr(self, 'lives_hud'):
+            self.canvas.itemconfig(self.lives_hud, text=text)
+        else:
+            self.lives_hud = self.draw_text(50, 20, text, 15)
+
+    def update_score_text(self):
+        score_text = f'Score: {self.score}'
+        if hasattr(self, 'score_hud'):
+            self.canvas.itemconfig(self.score_hud, text=score_text)
+        else:
+            self.score_hud = self.draw_text(550, 20, score_text, 15)
+
+    def start_game(self):
+        self.canvas.unbind('<space>')
+        self.canvas.delete(self.text)
+        self.paddle.ball = None
+        self.game_loop()
+
+    def game_loop(self):
+        self.check_collisions()
+        num_bricks = len(self.canvas.find_withtag('brick'))
+        if num_bricks == 0:
+            self.play_win_sound()  # Play win sound
+            self.draw_text(300, 200, 'You win! Breaker of Bricks!')
+        elif self.ball.get_position()[3] >= self.height:
+            self.lives -= 1
+            self.update_lives_text()
+            if self.lives < 0:
+                self.play_game_over_sound()  # Play game over sound
+                self.draw_text(300, 200, 'You Lose! Game Over!')
+            else:
+                self.after(1000, self.reset_ball)
+        else:
+            self.ball.update()
+            self.after(50, self.game_loop)
+
+    def check_collisions(self):
+        ball_coords = self.ball.get_position()
+        items = self.canvas.find_overlapping(*ball_coords)
+        objects = [self.items[x] for x in items if x in self.items]
+        self.ball.collide(objects)
+
+    def reset_ball(self):
+        #self.lives = 3
+        self.add_ball()
+        self.setup_game()
+
+    def play_win_sound(self):
+        pygame.mixer.Sound("win.mp3").play()  # Play win sound
+
+    def play_game_over_sound(self):
+        pygame.mixer.Sound("lose.mp3").play()  # Play game over sound
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Break those Bricks!')
+    game = Game(root)
+    game.mainloop()
